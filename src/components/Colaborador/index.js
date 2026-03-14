dd7bb946e82d12f67df7c71c3bdf403f0c19f552
vfluyu <vitor.008043@gmail.com>
2026-03-14 11:39:58 -0300

    Criação do card de colaborador

---
+++ src/components/Colaborador/index.js
@@ -0,0 +1,16 @@
+import './colaborador.css'
+
+function colaborador(props){
+    return(
+        <div className='colaborador'>
+            <div className='cabecalho'>
+                <img src={props.imagem} alt=''/>
+            </div>
+            <div className='rodape'>
+                <h4>{props.nome}</h4>
+                <h5>{props.cargo}</h5>
+            </div>
+        </div>
+    )
+}
+export default colaborador
--- src/components/Time/index.js
+++ src/components/Time/index.js
@@ -1,8 +1,17 @@
 import './Time.css'
+import Colaborador from '../Colaborador';
+
 function Time(props){
     return(
         <section className='time' style={{backgroundColor: props.corSecundaria}}>
             <h3 style={{borderBlockColor: props.corPrimaria}}>{props.nome}</h3>
+                <div className='colaboradores'>
+                    <Colaborador 
+                        nome="vfluyu" 
+                        cargo="Desenvolvedor" 
+                        imagem='https://github.com/vfluyu.png'
+                    />
+                </div>
         </section>
     )
 }
