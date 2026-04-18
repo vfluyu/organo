d926a99b6461242676d05c45deca8c3127556055
vfluyu <vitor.008043@gmail.com>
2026-04-18 19:49:44 -0300

    Erro rodape

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import Banner from './components/Banner';
 import Formulario from './components/Formulario';
 import Time from './components/Time';
-import Rodape from './Rodape';
+import Rodape from './components/Rodape';
 function App() {
 
   const times = [
